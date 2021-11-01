96244fd51ed9a95d540782dd62af8de655594b3b
silversurfer34 <fabian.serin@gmail.com>
2021-11-01 11:51:27 +0100

    timeit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# test_training_2021
+# Welcome to the Tests training  
+## Demos  
+### Unit performance tests using timeit  
+For the purpose of this demo we will compare several sort algorithms using timeit
+#### Prerequisites:
+* Python installed
+#### Timeit
+timeit will measure the time spent to execute some statements.
+We will use repeat method from timeit
+```
+times = repeat(setup=setup_code, stmt=stmt, repeat=1, number=1)
+```
+Timeit measure the time spent to run `stmt` executed `number` times.  
+repeat method runs it `repeat` times and return an array of measured time.
+
+#### Measuring performances
+`perfo/perfo.py` contains the code to run and measure the sorting algorithm.  
+The sorting algorithm are defined in `simple/sort.py`. You can define your own and test it
+
+#### Work to do
+- Define some test cases
+  - array is already sorted (1, 3, 5, 9, 11)
+  - array is already sorted but the min value is at the end (3, 5, 9, 11, 1)
+  - array is sorted but in reversed order (11, 9, 5, 3, 1)
+- For each of those determine which algorithm is the better for 10 000 values
+
---
+++ perfo/perfo.py
@@ -0,0 +1,48 @@
+from statistics import mean
+from timeit import repeat
+
+from sort.sort import *
+
+
+# inspired by https://realpython.com/sorting-algorithms-python/#pythons-built-in-sorting-algorithm
+
+
+def time_sorting_algorithm(algorithm, array):
+    # Set up the context and prepare the call to the specified
+    # algorithm using the supplied array. Only import the
+    # algorithm function if it's not the built-in `sorted()`.
+    setup_code = f"from __main__ import {algorithm}" if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time
+    # in seconds that each execution took
+    # number is the number of time stmt is called and measured
+    #   number=10 will measure the time spend for 10 launches of stmt
+    # repeat is the number of repetition of the measure
+    #   repeat=10 will measure 10 times and return 10 results
+    times = repeat(setup=setup_code, stmt=stmt, repeat=5, number=1)
+    # Finally, display the name of the algorithm and the
+    # minimum, max, average time it took to run
+    print(
+        f"Algorithm: {algorithm}. Minimum execution time: {min(times)}, Max execution time: {max(times)}, "
+        f"Average execution time: {mean(times)}"
+    )
+
+
+ARRAY_LENGTH = 1_000
+
+if __name__ == "__main__":
+    # Generate an array of `ARRAY_LENGTH` items consisting
+    # of random integer values between 0 and 999
+    random_array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    use_cases = {"random_array": random_array}
+    for label, array in use_cases.items():
+        # Call the function using the name of the sorting algorithm
+        # and the array you just created
+        print(label)
+        time_sorting_algorithm(algorithm="bubble_sort", array=array)
+        time_sorting_algorithm(algorithm="insertion_sort", array=array)
+        time_sorting_algorithm(algorithm="merge_sort", array=array)
+        time_sorting_algorithm(algorithm="quicksort", array=array)
